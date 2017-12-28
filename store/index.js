936ad7bd7ff851c22864bea3a8b38af5621abd18
corocn <crsk.plmzh@gmail.com>
2017-12-28 22:23:45 +0900

    add firebase and vuexfire with sample code

--- store/index.js
+++ store/index.js
@@ -1,13 +1,23 @@
+import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
+import { firebaseMutations, firebaseAction } from 'vuexfire'
+const db = firebase.database()
+const postsRef = db.ref('/posts')
+
+Vue.use(Vuex)
 
 const store = () => new Vuex.Store({
   state: {
-    counter: 0
+    posts: []
   },
   mutations: {
-    increment (state) {
-      state.counter++
-    }
+    ...firebaseMutations
+  },
+  actions: {
+    INIT_POSTS: firebaseAction(({ bindFirebaseRef }) => {
+      bindFirebaseRef('posts', postsRef)
+    })
   }
 })
 
