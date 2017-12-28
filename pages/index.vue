ab4451e5ca1be15cc4b4e8413a21bd0eb7275166
corocn <crsk.plmzh@gmail.com>
2017-12-28 22:14:59 +0900

    Vuex追加

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
+      <button @click="$store.commit('increment')">{{ $store.state.counter }}</button>
     </div>
   </section>
 </template>
