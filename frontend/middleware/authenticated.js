63b5da47ffb1c3522e51486c5817ea051183db80
corocn <crsk.plmzh@gmail.com>
2018-01-06 11:22:35 +0900

    add comment

--- frontend/middleware/authenticated.js
+++ frontend/middleware/authenticated.js
@@ -1,4 +1,5 @@
 export default function ({ store, redirect }) {
+  // TODO: 非認証系のときにOFFる
   store.$axios.setHeader('Authorization', 'Bearer ' + store.getters.idToken)
 
   if (!store.getters.isAuthenticated) {
